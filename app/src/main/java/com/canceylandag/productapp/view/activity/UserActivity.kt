6e05455c2258502d32c7d837a72db511c126c42c
Can Ceylandağ <69530509+canceylandag@users.noreply.github.com>
2023-11-15 22:19:03 +0300

    menubar a başlandı

--- app/src/main/java/com/canceylandag/productapp/view/activity/UserActivity.kt
+++ app/src/main/java/com/canceylandag/productapp/view/activity/UserActivity.kt
@@ -2,11 +2,21 @@ package com.canceylandag.productapp.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.canceylandag.productapp.R
+
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+       /* val navController=supportFragmentManager.findFragmentById(R.id.nav_products) as NavHostFragment
+        val appBarConfiguration= AppBarConfiguration(navController.navController.graph)
+        findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar).setupWithNavController(navController.navController,appBarConfiguration)*/
     }
 }
